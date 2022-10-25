fa4aed862aacc6cfbdab41398f0a43f68eab67ad
Oleg Fedotov <trevor706@gmail.com>
2022-10-25 13:00:38 +0300

    Initial Commit

---
+++ Contact List/Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_ListApp.swift
+//  Contact List
+//
+//  Created by Olegio on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
